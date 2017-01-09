fe1c076968236c1e4c5b7319d010010caf81e2d4
Kory Skarbek and Melvin Gruschow <melvin@>
2017-01-09 09:39:58 -0800

    Add subtract multiply divide functions

--- js/basicCalculator.js
+++ js/basicCalculator.js
@@ -2,9 +2,19 @@ var add = function(number1, number2) {
 	return number1 + number2;
 };
 
-//alert(add(10, 5))
+var subtract = function(number1, number2){
+	return number1 - number2;
+}
+
+var multiply = function(number1, number2){
+	return number1 * number2;
+}
+
+var divide = function(number1, number2){
+	return number1 / number2;
+}
 
 var numb1 = parseInt(prompt("Enter a number:"));
 var numb2 = parseInt(prompt("Enter another number:"));
 
-alert(add(numb1, numb2));
+alert(subtract(numb1, numb2));
