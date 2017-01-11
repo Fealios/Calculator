468c872121fcf80ea81790614ae1a65799cb40eb
Melvin Gruschow and said ahmed <melvin@>
2017-01-11 11:10:13 -0800

    redo the calculator page and make it use forms instead of prompts and alerts

--- calculator/js/basicCalculator.js
+++ calculator/js/basicCalculator.js
@@ -1,20 +1,26 @@
 var add = function(number1, number2) {
-	return number1 + number2;
+  return number1 + number2;
 };
 
-var subtract = function(number1, number2){
-	return number1 - number2;
-}
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
 
-var multiply = function(number1, number2){
-	return number1 * number2;
-}
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
 
-var divide = function(number1, number2){
-	return number1 / number2;
-}
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
 
-var numb1 = parseInt(prompt("Enter a number:"));
-var numb2 = parseInt(prompt("Enter another number:"));
-
-alert(subtract(numb1, numb2));
+$(document).ready(function() {
+  $("#add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#add1").val());
+    var number2 = parseInt($("#add2").val());
+    // alert(add(number1, number2));
+		var result = add(number1, number2);
+		$("#output").text(result);
+  });
+});
