00f17c14f67a04e6a85d6f0f86aeaf7c220111df
melvin gruschow and sandro mijatovic <melvin@>
2017-01-12 13:13:41 -0800

    make calculator function with radio buttons and if statements

--- calculator/js/basicCalculator.js
+++ calculator/js/basicCalculator.js
@@ -17,10 +17,22 @@ var divide = function(number1, number2) {
 $(document).ready(function() {
   $("#add").submit(function(event) {
     event.preventDefault();
+
+    var operator = $('input:radio[name=operator]:checked').val();
+
     var number1 = parseInt($("#add1").val());
     var number2 = parseInt($("#add2").val());
     // alert(add(number1, number2));
-		var result = add(number1, number2);
+    var result ;
+    if (operator === "add") {
+   result = add(number1, number2);
+ } else if (operator === "subtract") {
+   result = subtract(number1, number2);
+ } else if (operator === 'multiply') {
+   result = multiple(number1, number2);
+ } else if (operator === 'divide') {
+   result = divide(number1, number2);
+ }
 		$("#output").text(result);
   });
 });
